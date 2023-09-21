7b6b2a698f2f909fa18724a9e1975eca49b47228
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-09-21 09:00:41 +0630

    frontend(react): add react-toastify package

--- qrmenu_frontend/src/router/App.jsx
+++ qrmenu_frontend/src/router/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
