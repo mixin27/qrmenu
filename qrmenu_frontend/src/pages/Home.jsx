41cd2220cf9621c6848c8bfed5cc94a59f76ac71
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-09-22 11:35:23 +0630

    feat(home): more interactive home page design

--- qrmenu_frontend/src/pages/Home.jsx
+++ qrmenu_frontend/src/pages/Home.jsx
@@ -1,15 +1,39 @@
+import { Button, Container, Row, Col, Image } from "react-bootstrap";
+
 import MainLayout from "../layout/MainLayout";
 
 const Home = () => {
   return (
     <MainLayout>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut aspernatur,
-        doloribus vitae doloremque iure aut culpa minima? Aut nobis eveniet
-        ducimus deleniti ipsum repellendus blanditiis! Consequuntur perspiciatis
-        animi officia voluptatem!
-      </p>
+      <div className="p-5 mb-4 bg-light rounded-3">
+        <Container fluid className="py-5">
+          <Row>
+            <Col md={6} className="my-auto">
+              <h1 className="display-5 fw-bold">QR Code Menu</h1>
+              <p className="mt-4 mb-4 fs-4">
+                A smart way to share your digital menu in a QR code with your
+                customers
+              </p>
+              <br />
+              <Button
+                href="/places"
+                variant="primary"
+                size="lg"
+                className="my-4"
+              >
+                Create Your Menu
+              </Button>
+            </Col>
+            <Col md={6}>
+              <Image
+                src="https://assets.materialup.com/uploads/ae60e834-349c-4c94-8189-2450f09ad37a/preview.gif"
+                rounded
+                fluid
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </MainLayout>
   );
 };
